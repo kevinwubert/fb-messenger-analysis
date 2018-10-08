3f4437c0d87a1dc6220af7713761189d164d40d4
Kevin Wu <kevinwubert@gmail.com>
2018-10-08 02:58:58 -0700

    switched to port 80

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -32,6 +32,6 @@ func Main() error {
 	http.HandleFunc("/topSticker", visualizerClient.TopStickerHandler)
 	http.HandleFunc("/getNames", visualizerClient.GetNamesHandler)
 
-	err = http.ListenAndServe(":8080", nil)
+	err = http.ListenAndServe(":80", nil)
 	return err
 }
